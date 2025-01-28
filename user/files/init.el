ff15b9e3dc260c6892d423d4883c23bea2530547
Emacs is my life <emacs.is.my.life@gmail.com>
2025-01-29 02:28:48 +0900

    1234

--- user/files/init.el
+++ user/files/init.el
@@ -1824,80 +1824,6 @@
 
 
 
-;; # ==== [LLM assistance]
-
-
-
-
-(defun gptel/get-llm-providers ()
-  (progn
-    (setq auth-sources `(,(concat (getenv "USER_SECRET_DIR") "authinfo.gpg")))
-    (let ((service-provider-list '("generativelanguage.googleapis.com" "api.perplexity.ai" "api.anthropic.com" "api.groq.com" "api.deepseek.com" "api.x.ai"))
-	        (providers-list '()))
-      (dolist (provider service-provider-list)
-	      (let ((auth-info (car (auth-source-search :max 1
-						                                      :host provider
-						                                      :user "apikey"
-						                                      :require '(:secret)))))
-	        (if auth-info
-	            (let ((apikey (plist-get auth-info :secret)))
-	              (cond
-		             ((stringp apikey) (push (cons provider apikey) providers-list))
-		             ((functionp apikey) (push (cons provider (funcall apikey)) providers-list)))))))
-      providers-list)))
-
-(use-package gptel
-  :ensure t
-  :config
-  (dolist (provider-info (gptel/get-llm-providers))
-    (let ((provider (car provider-info))
-	        (apikey (cdr provider-info)))
-	    (cond
-	     ((string= provider "generativelanguage.googleapis.com")
-	      (gptel-make-gemini "Gemini"
-			    :key apikey
-			    :stream t))
-	     ((string= provider "api.perplexity.ai")
-	      (gptel-make-perplexity "Perplexity"
-				                       :key apikey
-				                       :stream t))
-	     ((string= provider "api.anthropic.com")
-	      (gptel-make-anthropic "Claude"
-				  :key apikey
-				  :stream t))
-	     ((string= provider "api.groq.com")
-	      (gptel-make-openai "Groq"
-			    :key apikey
-			    :host "api.groq.com"
-			    :endpoint "/openai/v1/chat/completions"
-			    :stream t
-			    :models '(llama-3.1-70b-versatile
-					          llama-3.1-8b-instant
-					          llama3-70b-8192
-					          llama3-8b-8192
-					          mixtral-8x7b-32768
-					          gemma-7b-it)))
-	     ((string= provider "api.deepseek.com")
-	      (gptel-make-openai "Deepseek"
-			    :key apikey
-			    :host "api.deepseek.com"
-			    :endpoint "/chat/completions"
-			    :stream t
-			    :models '(deepseek-chat deepseek-coder)))
-	     ((string= provider "api.x.ai")
-	      (gptel-make-openai "xAI"
-			    :key apikey
-			    :host "api.x.ai"
-			    :endpoint "/v1/chat/completions"
-			    :stream t
-			    :models '(grok-beta)))))))
-
-
-
-
-
-
-
 ;; # ==== [Guix]
 
 
@@ -2140,4 +2066,72 @@
 
 
 
+
+;; # ==== [LLM assistance]
+
+
+
+
+(defun gptel/get-llm-providers ()
+  (progn
+    (setq auth-sources `(,(concat (getenv "USER_SECRET_DIR") "authinfo.gpg")))
+    (let ((service-provider-list '("generativelanguage.googleapis.com" "api.perplexity.ai" "api.anthropic.com" "api.groq.com" "api.deepseek.com" "api.x.ai"))
+	        (providers-list '()))
+      (dolist (provider service-provider-list)
+	      (let ((auth-info (car (auth-source-search :max 1
+						                                      :host provider
+						                                      :user "apikey"
+						                                      :require '(:secret)))))
+	        (if auth-info
+	            (let ((apikey (plist-get auth-info :secret)))
+	              (cond
+		             ((stringp apikey) (push (cons provider apikey) providers-list))
+		             ((functionp apikey) (push (cons provider (funcall apikey)) providers-list)))))))
+      providers-list)))
+
+(use-package gptel
+  :ensure t
+  :config
+  (dolist (provider-info (gptel/get-llm-providers))
+    (let ((provider (car provider-info))
+	        (apikey (cdr provider-info)))
+	    (cond
+	     ((string= provider "generativelanguage.googleapis.com")
+	      (gptel-make-gemini "Gemini"
+			    :key apikey
+			    :stream t))
+	     ((string= provider "api.perplexity.ai")
+	      (gptel-make-perplexity "Perplexity"
+				                       :key apikey
+				                       :stream t))
+	     ((string= provider "api.anthropic.com")
+	      (gptel-make-anthropic "Claude"
+				  :key apikey
+				  :stream t))
+	     ((string= provider "api.groq.com")
+	      (gptel-make-openai "Groq"
+			    :key apikey
+			    :host "api.groq.com"
+			    :endpoint "/openai/v1/chat/completions"
+			    :stream t
+			    :models '(llama-3.1-70b-versatile
+					          llama-3.1-8b-instant
+					          llama3-70b-8192
+					          llama3-8b-8192
+					          mixtral-8x7b-32768
+					          gemma-7b-it)))
+	     ((string= provider "api.deepseek.com")
+	      (gptel-make-openai "Deepseek"
+			    :key apikey
+			    :host "api.deepseek.com"
+			    :endpoint "/chat/completions"
+			    :stream t
+			    :models '(deepseek-chat deepseek-coder)))
+	     ((string= provider "api.x.ai")
+	      (gptel-make-openai "xAI"
+			    :key apikey
+			    :host "api.x.ai"
+			    :endpoint "/v1/chat/completions"
+			    :stream t
+			    :models '(grok-beta)))))))
 
