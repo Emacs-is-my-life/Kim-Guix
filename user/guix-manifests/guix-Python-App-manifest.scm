bae7ab545805b298c0715c88e4cc9ce37b364226
Emacs is my life <emacs.is.my.life@gmail.com>
2025-01-30 00:04:33 +0900

    134

---
+++ user/guix-manifests/guix-Python-App-manifest.scm
@@ -0,0 +1,9 @@
+;; Profile for Python general application development
+(specifications->manifest
+ '(
+   "python-rich"
+   "python-rich-click"
+   "python-rich-tables"
+   "python-rich-toolkit"
+   "python-pyqt"
+))
