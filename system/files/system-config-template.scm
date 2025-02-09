17728f0018ca1a72c742dbcfa52a38885e7d6a40
Emacs is my life <emacs.is.my.life@gmail.com>
2025-02-09 10:57:16 +0900

    Added libvdpau-va-gl

--- system/files/system-config-template.scm
+++ system/files/system-config-template.scm
@@ -141,7 +141,7 @@
                     coreutils binutils findutils diffutils iputils sysfsutils efibootmgr tar zip unzip bash-completion vim git
 
                     ;; video
-                    mesa mesa-utils libva libva-utils intel-vaapi-driver
+                    mesa mesa-utils libva libva-utils intel-vaapi-driver libvdpau-va-gl
 
                     ;; autio
                     alsa-lib alsa-utils alsa-plugins pulseaudio pavucontrol
