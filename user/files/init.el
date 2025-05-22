3d32ce2d6b71209cfcd1491aa73081278b4ca1bf
Emacs is my life <emacs.is.my.life@gmail.com>
2025-05-22 15:34:47 +0900

    lsp-ui peek

--- user/files/init.el
+++ user/files/init.el
@@ -1594,6 +1594,8 @@ DEADLINE: %^{Deadline}t
   :after lsp-mode 
   :commands lsp-ui-mode
   :config
+  (define-key lsp-ui-mode-map [remap xref-find-definitions] #'lsp-ui-peek-find-definitions)
+  (define-key lsp-ui-mode-map [remap xref-find-references] #'lsp-ui-peek-find-references)
   (setq lsp-enable-symbol-highlighting t)
   (setq lsp-ui-doc-enable t)
   (setq lsp-ui-doc-header t)
