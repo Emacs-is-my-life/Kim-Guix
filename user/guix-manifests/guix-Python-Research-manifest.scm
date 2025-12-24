efbcafa125e2586ebe78b3f4b2ff3de131a04d26
GUIXXER <emacs.is.my.life@gmail.com>
2025-12-24 19:49:09 +0900

    Polished marimo

--- user/guix-manifests/guix-Python-Research-manifest.scm
+++ user/guix-manifests/guix-Python-Research-manifest.scm
@@ -1,8 +1,11 @@
 ;; Profile for Python numerical and symbolic computation packages
 (specifications->manifest
  '(
-   "python-numpy"
+   ;; "python-numpy"
    "python-scipy"
    "python-sympy"
    "python-tqdm"
+   "python-numba"
+   "python-statsmodels"
+   "python-kiwisolver"
    ))
