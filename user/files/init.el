c37cedbcd99ef93c508abede095eba8d9ee40c3a
GUIXXER <emacs.is.my.life@gmail.com>
2026-03-04 19:11:18 +0900

    Reverted model selection back to gpt 5.1 chat

--- user/files/init.el
+++ user/files/init.el
@@ -2696,8 +2696,8 @@ Replace <your-expressions-here> with mathematical expressions written in LaTeX g
 			(setq gptel-backend (gptel-make-openai "ChatGPT"
 								  :key apikey
 								  :stream t
-								  :models '(gpt-5-mini gpt-5.1-chat-latest o4-mini-deep-research))
-				  gptel-model 'gpt-5-mini))
+								  :models '(gpt-5.1-chat-latest gpt-5.1-codex-max o4-mini-deep-research))
+				  gptel-model 'gpt-5.1-chat-latest))
 		   ((string= provider "generativelanguage.googleapis.com")
 			(setq gptel-backend (gptel-make-gemini "Gemini"
 								  :key apikey
@@ -2785,7 +2785,7 @@ Replace <your-expressions-here> with mathematical expressions written in LaTeX g
 	  :custom
 	  (aidermacs-default-chat-mode 'architect)
 	  ;; Model Selection
-	  (aidermacs-default-model "openai/gpt-5-mini")
+	  (aidermacs-default-model "openai/gpt-5.1-chat-latest")
 	  (aidermacs-architect-model "openai/o3-mini")
 	  (aidermacs-editor-model "openai/gpt-5.1-codex-max")
 	  (aidermacs-weak-model "openai/gpt-4o-mini")))
