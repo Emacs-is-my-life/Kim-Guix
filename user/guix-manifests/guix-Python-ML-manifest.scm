91240036ee017757808be15c2409bad72a083cd5
GUIXXER <emacs.is.my.life@gmail.com>
2026-03-15 14:10:04 +0900

    Added python packages

--- user/guix-manifests/guix-Python-ML-manifest.scm
+++ user/guix-manifests/guix-Python-ML-manifest.scm
@@ -1,6 +1,8 @@
 ;; Profile for Python machine learning packages
 (specifications->manifest
  '(
+   "python-ml-dtypes"
+   "python-ml-collections"
    "python-scikit-learn"
    "python-xgboost"
-))
+   ))
---
+++ user/guix-manifests/guix-Python-STL-manifest.scm
@@ -0,0 +1,7 @@
+;; Profile for Python Data Structures & Algorithms
+(specifications->manifest
+ '(
+   "python-cachetools"
+   "python-sortedcontainers"
+   "python-more-itertools"
+   ))
